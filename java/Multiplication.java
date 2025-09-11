73bf08ee3222ff626882593ff4198bab087cb5b4
nakulswami <nakulswami92000@gmail.com>
2025-09-11 10:42:09 +0530

    update in multiplication

--- java/Multiplication.java
+++ java/Multiplication.java
@@ -1,9 +1,16 @@
-public class Multiplication {
-    public static void main(String[] args) {
-        int n = 5;
 
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(n + " x " + i + " = " + (n * i));
+public class Multiplication {
+
+    public static void main(String[] args) {
+        int n = 10;
+        if (n == 0) {
+            System.out.println("Not valid for multiplication");
+
+        } else {
+
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(n + " x " + i + " = " + (n * i));
+            }
         }
     }
 }
