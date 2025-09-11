16d27cf2ce012111a268692a035ad8cf78b4921b
nakulswami <nakulswami92000@gmail.com>
2025-09-11 09:47:07 +0530

    loops and switch cases

---
+++ java/Count.java
@@ -0,0 +1,8 @@
+//wap to print 1 to 10 using for loop
+public class Count {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
